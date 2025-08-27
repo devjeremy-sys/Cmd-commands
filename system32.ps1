9a7e1773fbd1aa276741e757340ef3bc7add158e
devjeremy-sys <dev.jeremy.gonzalo@gmail.com>
2025-08-27 15:28:10 +0200

    Update system32.ps1

--- system32.ps1
+++ system32.ps1
@@ -1,4 +1,18 @@
-Write-Host "Made by Jeremy`n"
+Clear-Host
+Clear-Host
+Write-Host @"
+[38;2;160;32;240;48;2m  _   _                     _____ ____ ___  
+ | \ | |                   / ____|___ \__ \ 
+ |  \| | __ _ _ __ ___ ___| (___   __) | ) |
+ |  . ` |/ _` | '__/ __/ _ \\___ \ |__ < / / 
+ | |\  | (_| | | | (_| (_) |___) |___) / /_ 
+ |_| \_|\__,_|_|  \___\___/_____/|____/_____[0m
+                                            
+                                            
+
+"@ -ForegroundColor Magenta
+
+Write-Host "Made by Carpel for NarcoCity`n"
 
 $system32Path = "$env:SystemRoot\System32"
 
